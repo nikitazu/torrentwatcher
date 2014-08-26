bd271e7764c57846ee96c65ab711772ed64ba5bf
Nikita B. Zuev <nikitazu@gmail.com>
2014-08-26 23:58:21 +0400

    Can handle nil scores

--- app/helpers/animes_helper.rb
+++ app/helpers/animes_helper.rb
@@ -2,7 +2,7 @@
 module AnimesHelper
   def score_tag(score)
     total_stars = 10
-    black_stars = score
+    black_stars = score || 0
     white_stars = total_stars - black_stars
     content_tag :span, "★" * black_stars + "☆" * white_stars
   end
