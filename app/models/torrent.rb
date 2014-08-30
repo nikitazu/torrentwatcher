003f46bc2e0cffe0ec96d3b2cd0a67974e476f06
Nikita B. Zuev <nikitazu@gmail.com>
2014-08-30 13:18:58 +0400

    Better parsing

--- app/models/torrent.rb
+++ app/models/torrent.rb
@@ -2,7 +2,8 @@ class Torrent
   attr_accessor :title, :name, :link, :description,
     :group, :group_type, :weight,
     :seeders, :leechers, :downloads, :size, :size_units,
-    :is_trusted, :is_remake, :is_a_plus
+    :is_trusted, :is_remake, :is_a_plus,
+    :resolution, :episode
   
   def initialize(item)
     self.title = item.title
@@ -10,6 +11,7 @@ class Torrent
     self.description = item.description
     self.name = self.title
     
+    pre_clean_name!
     group_matcher = /^\[(\S+)\][\s|_]/
     group_match = group_matcher.match(self.title)
     self.group = group_match ? group_match[1] : ""
@@ -17,6 +19,9 @@ class Torrent
     parse_group!
     parse_seeders!
     parse_ratings!
+    parse_resolution!
+    post_clean_name!
+    parse_episode!
   end
   
   def <=>(other)
@@ -24,6 +29,21 @@ class Torrent
   end
   
   private
+    def pre_clean_name!
+      self.name.gsub! /\d+x1080/, '1080p'
+      self.name.gsub! /\d+x720/, '720p'
+      self.name.gsub! /\d+x480/, '480p'
+    end
+    
+    def post_clean_name!
+      self.name.gsub! /(\[|\(|\s)(AAC|10-?bit|H264)(\]|\)|\s)/, ''
+      self.name.gsub! /\.(mkv|avi|mp4)/, ''
+      self.name.gsub! /_/, ' '
+      self.name.gsub! /\[\]/, ''
+      self.name.gsub! /\(\)/, ''
+      self.name.gsub! /\[\w+\]/, ''
+    end
+    
     def parse_group!
       if /(HorribleSubs)|(Commie)/.match(self.group)
         self.group_type = "torrent-group-good"
@@ -54,5 +74,21 @@ class Torrent
       self.is_trusted = /Trusted/.match(self.description)
       self.is_remake = /Remake/.match(self.description)
       self.is_a_plus = /A\+/.match(self.description)
+    end
+    
+    def parse_resolution!
+      title_matcher = /((1080p)|(720p)|(480p))/
+      title_match = title_matcher.match(self.name)
+      if title_match
+        self.name.gsub! title_matcher, ''
+        self.resolution = title_match[1]
+      end
+    end
+    
+    def parse_episode!
+      title_match = /[\s|\(|\[|-](\d{1,3})[$|\)|\]|\s|v]/.match(self.name)
+      if title_match
+        self.episode = title_match[1].to_i
+      end
     end
 end
